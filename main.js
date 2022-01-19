2c3d5692d8685b456803bd0681e48d609ac076ae
Alessandro Terracciano <terracciano41@gmail.com>
2022-01-19 14:55:37 +0100

    Struttura html e prompt

---
+++ main.js
@@ -0,0 +1,8 @@
+/* PASSWORD GENERATOR */
+
+
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
+let colore = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById('bersaglio').innerHTML = "";
