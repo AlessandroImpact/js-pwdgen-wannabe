464ddcb647837b901f89a55ba10ff0a279f34a46
Alessandro Terracciano <terracciano41@gmail.com>
2022-01-19 15:07:02 +0100

    es risolto

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 /* PASSWORD GENERATOR */
 
+/*alert di benvenuto */
+alert("Benvenuto! Premi invio per generare una nuova password");
+
 
 let nome = prompt("Inserisci il tuo nome");
 let cognome = prompt("Inserisci il tuo cognome");
 let colore = prompt("Inserisci il tuo colore preferito");
+let numero = 19;
 
-document.getElementById('bersaglio').innerHTML = "";
+let pass ="la password generata è " + nome + cognome + colore + numero;
+document.getElementById('bersaglio').innerHTML = pass;
