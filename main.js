e55380cb40a60183334bb7944327cff644110ddf
Alessandro Terracciano <terracciano41@gmail.com>
2022-01-19 15:11:57 +0100

    aggiunta commenti

--- main.js
+++ main.js
@@ -3,11 +3,18 @@
 /*alert di benvenuto */
 alert("Benvenuto! Premi invio per generare una nuova password");
 
-
+/*richiesta nome */
 let nome = prompt("Inserisci il tuo nome");
-let cognome = prompt("Inserisci il tuo cognome");
-let colore = prompt("Inserisci il tuo colore preferito");
-let numero = 19;
 
+/*richiesta cognome */
+let cognome = prompt("Inserisci il tuo cognome");
+
+/*richiesta colore */
+let colore = prompt("Inserisci il tuo colore preferito");
+
+/*variabile numero */
+let numero = 21;
+
+/*Inserimento e stampa in html */
 let pass ="la password generata è " + nome + cognome + colore + numero;
 document.getElementById('bersaglio').innerHTML = pass;
